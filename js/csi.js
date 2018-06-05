540f0a2e3270adbe880985683624b0d0b1f6f1c2
cramdesign <matt@cramdesign.com>
2018-06-05 16:29:03 -0400

    Update csi.js

--- js/csi.js
+++ js/csi.js
@@ -1,13 +1,13 @@
 /* https://github.com/LexmarkWeb/csi.js */
-
 window.onload = function() {
-	var elements = document.getElementsByTagName('*'),
-		i;
+	
+	var elements = document.getElementsByTagName('*'), i;
 	for (i in elements) {
 		if (elements[i].hasAttribute && elements[i].hasAttribute('data-include')) {
 			fragment(elements[i], elements[i].getAttribute('data-include'));
 		}
 	}
+	
 	function fragment(el, url) {
 		var localTest = /^(?:file):/,
 			xmlhttp = new XMLHttpRequest(),
@@ -15,15 +15,9 @@ window.onload = function() {
 
 		xmlhttp.onreadystatechange = function() {
 			/* if we are on a local protocol, and we have response text, we'll assume things were sucessful */
-			if (xmlhttp.readyState == 4) {
-				status = xmlhttp.status;
-			}
-			if (localTest.test(location.href) && xmlhttp.responseText) {
-				status = 200;
-			}
-			if (xmlhttp.readyState == 4 && status == 200) {
-				el.outerHTML = xmlhttp.responseText;
-			}
+			if (xmlhttp.readyState == 4) status = xmlhttp.status;
+			if (localTest.test(location.href) && xmlhttp.responseText) status = 200;
+			if (xmlhttp.readyState == 4 && status == 200) el.outerHTML = xmlhttp.responseText;
 		}
 
 		try { 
